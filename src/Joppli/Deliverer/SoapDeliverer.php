a9d545f7c3f242113d3923686ce306c9ba9235d1
Erik Landvall <erik@landvall.se>
2017-11-30 10:53:28 +0100

    Update SoapDeliverer.php

--- src/Joppli/Deliverer/SoapDeliverer.php
+++ src/Joppli/Deliverer/SoapDeliverer.php
@@ -5,17 +5,26 @@ namespace Joppli\Deliverer;
 use
 SoapFault,
 SoapServer,
+Joppli\Builder\Aware\BuilderAware,
+Joppli\Builder\Aware\BuilderAwareTrait,
 Joppli\Request\Aware\RequestAware,
 Joppli\Request\Aware\RequestAwareTrait;
 
-class SoapDeliverer extends AbstractDeliverer implements RequestAware
+class SoapDeliverer extends AbstractDeliverer
+implements RequestAware, BuilderAware
 {
-  use RequestAwareTrait;
+  use
+  BuilderAwareTrait,
+  RequestAwareTrait;
 
   protected function composeOutput(array $entity) : string
   {
+    $obj = is_string($entity['soap'])
+         ? $this->builder->build($entity['soap'])
+         : $entity['soap'];
+
     $server = new SoapServer(null, ['uri' =>  $this->request->getUri()]);
-    $server->setObject($entity['soap']);
+    $server->setObject($obj);
     $server->handle();
     return '';
   }
